a206ba0bdebb941deaa3536b2d02fa6712ba9dc5
SEO Book Pro <79488264+seobookpro@users.noreply.github.com>
2023-05-09 19:49:44 +0300

    Create clear.js
    Add Clear JavaScript Functionality for the Style Plugin Settings and Options

---
+++ inc/assets/js/clear.js
@@ -0,0 +1,36 @@
+function clearSliderValueMarginTop() {
+  // Set the value of the range slider to its default or desired initial value
+  document.querySelector('input[name="seobpsbfw_share_container_margin_top"]').value = 0;
+
+  // Call the function to update the slider value display
+  updateSliderValueMarginTop(0);
+}
+
+function clearSliderValueMarginRight() {
+  // Set the value of the range slider to its default or desired initial value
+  document.querySelector('input[name="seobpsbfw_share_container_margin_right"]').value = 0;
+
+  // Call the function to update the slider value display
+  updateSliderValueMarginRight(0);
+}
+
+
+function clearSliderValueMarginBottom() {
+  // Set the value of the range slider to its default or desired initial value
+  document.querySelector('input[name="seobpsbfw_share_container_margin_bottom"]').value = 0;
+
+  // Call the function to update the slider value display
+  updateSliderValueMarginBottom(0);
+}
+
+
+
+function clearSliderValueMarginLeft() {
+  // Set the value of the range slider to its default or desired initial value
+  document.querySelector('input[name="seobpsbfw_share_container_margin_left"]').value = 0;
+
+  // Call the function to update the slider value display
+  updateSliderValueMarginLeft(0);
+}
+
+
